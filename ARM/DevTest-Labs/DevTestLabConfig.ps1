de9aebf311151a5fccc23bac6f5341d48b14143d
Daniel Neumann <mail@neumanndaniel.de>
2016-12-08 15:50:18 +0100

    Add files via upload

---
+++ ARM/DevTest-Labs/DevTestLabConfig.ps1
@@ -0,0 +1,8 @@
+﻿$ResourceGroup="TS16Lab"
+$LabName="TS16Lab"
+
+Set-AzureRmDtlAllowedVMSizesPolicy -ResourceGroupName $ResourceGroup -LabName $LabName -VmSizes "Standard_F1","Standard_A2_v2","Standard_A1_v2","Standard_D1_v2" -Enable -Verbose
+
+Set-AzureRmDtlVMsPerLabPolicy -ResourceGroupName $ResourceGroup -LabName $LabName -MaxVMs 100 -Enable -Verbose
+
+Set-AzureRmDtlVMsPerUserPolicy -ResourceGroupName $ResourceGroup -LabName $LabName -MaxVMs 10 -Enable -Verbose
